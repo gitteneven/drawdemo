399529ab14afd04844f51a44f4a5fd71ba27b4d4
Gitte <gitte.neven@student.howest.be>
2022-06-06 17:47:46 +0200

    colors can be changed

--- sketch.js
+++ sketch.js
@@ -1,10 +1,47 @@
+// let colorPicker;
+let color;
+color = '#000000';
+
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+  createCanvas(500, 500);
   strokeWeight(10);
-  stroke(0);
+//   colorPicker = createColorPicker('#ed225d');
+//   colorPicker.position(0, height + 5);
+  
 }
 
-function draw() {
+function touchMoved() {
   line(mouseX, mouseY, pmouseX, pmouseY);
+    stroke(color);
+
   return false;
+//   background(colorPicker.color());
 }
+
+const handleUndoClick = e => {
+    e.preventDefault(); 
+    console.log('undo')
+}
+
+// const handlePencilClick = e => {
+//     e.preventDefault();
+//     console.log('pencil');
+//     touchMoved();
+// }
+
+const handleColorChange = e => {
+    e.preventDefault();
+    console.log(e.target.value);
+    color= e.target.value;
+}
+
+const init = () => {
+    const $undo = document.querySelector('.undo')
+    $undo.addEventListener('click', handleUndoClick);
+    const $color = document.querySelector('.color');
+    $color.addEventListener('change', handleColorChange);
+    // const $pencil = document.querySelector('.pencil');
+    // $pencil.addEventListener('click', handlePencilClick);
+}
+
+init();
