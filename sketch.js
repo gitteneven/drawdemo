bcde1a5abd89645166996861d7b6b04606d8af54
Gitte <gitte.neven@student.howest.be>
2022-06-07 10:54:38 +0200

    touch event instead of changeevent

--- sketch.js
+++ sketch.js
@@ -1,21 +1,22 @@
 // let colorPicker;
 let color;
 color = '#000000';
+let previousState;
+let stateIndex = 0;
+
 
 function setup() {
   createCanvas(500, 500);
   strokeWeight(10);
-//   colorPicker = createColorPicker('#ed225d');
-//   colorPicker.position(0, height + 5);
-  
+    // save state at the beginning for blank canvas 
+    // saveState();
+
 }
 
 function touchMoved() {
   line(mouseX, mouseY, pmouseX, pmouseY);
     stroke(color);
-
   return false;
-//   background(colorPicker.color());
 }
 
 const handleUndoClick = e => {
@@ -39,7 +40,7 @@ const init = () => {
     const $undo = document.querySelector('.undo')
     $undo.addEventListener('click', handleUndoClick);
     const $color = document.querySelector('.color');
-    $color.addEventListener('change', handleColorChange);
+    $color.addEventListener('touchstart', handleColorChange);
     // const $pencil = document.querySelector('.pencil');
     // $pencil.addEventListener('click', handlePencilClick);
 }
